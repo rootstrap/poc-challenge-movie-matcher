5c1def8b10f3e437b6ee430951bd436c933bb71b
Juan Pablo Irazusta <juanpabloirazusta@gmail.com>
2022-11-16 16:01:05 -0300

    Change FlatList to map

--- src/components/AvatarPile/AvatarPile.tsx
+++ src/components/AvatarPile/AvatarPile.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { RecommendedBy } from 'types';
 
 import Image from 'components/Image';
@@ -12,8 +12,6 @@ interface AvatarPileProps {
 
 const MAX_AVATARS = 5;
 
-const renderItem = ({ item }: any) => <Image source={{ uri: item.avatar }} style={styles.avatar} />;
-
 const AvatarPile = ({ people, ...props }: AvatarPileProps) => {
   const data = people.slice(0, MAX_AVATARS);
   return (
@@ -21,13 +19,9 @@ const AvatarPile = ({ people, ...props }: AvatarPileProps) => {
       {!!data.length && (
         <View style={styles.container}>
           <Image source={{ uri: data[0].avatar }} style={[styles.avatar, styles.firstAvatar]} />
-          <FlatList
-            horizontal
-            data={data}
-            keyExtractor={item => item.name}
-            renderItem={renderItem}
-            showsHorizontalScrollIndicator={false}
-          />
+          {data.map(({ avatar }) => (
+            <Image key={avatar} source={{ uri: avatar }} style={styles.avatar} />
+          ))}
         </View>
       )}
       <Text accessibilityRole="text">
