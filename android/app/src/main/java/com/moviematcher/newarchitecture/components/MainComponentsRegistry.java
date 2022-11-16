5847b59fa26ed20fe98a48974922ac236fa477ca
Manuela Viola <manuela.viola@rootstrap.com>
2022-11-16 15:24:39 +0100

    chore: rename app

--- android/app/src/main/java/com/moviematcher/newarchitecture/components/MainComponentsRegistry.java
+++ android/app/src/main/java/com/moviematcher/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.reactnativebase.newarchitecture.components;
+package com.moviematcher.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
