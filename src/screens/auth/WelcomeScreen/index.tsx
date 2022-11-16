2916c6e8a4bd394234a2c5e901f3ee9eb8a894e0
aguskoll <aguskoll@gmail.com>
2022-11-16 12:24:56 -0300

    initial activity and IntegrationModule.kt

--- src/screens/auth/WelcomeScreen/index.tsx
+++ src/screens/auth/WelcomeScreen/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { SafeAreaView, StatusBar, Text, TouchableOpacity, useColorScheme } from 'react-native';
+import { SafeAreaView, StatusBar, Text, TouchableOpacity, useColorScheme, NativeModules } from 'react-native';
 import RNBootSplash from 'react-native-bootsplash';
 
 import { AppScreens, NativeStackScreenProps, StackParamList } from 'navigation/types';
@@ -8,8 +8,14 @@ import styles from './styles';
 
 interface PropTypes extends NativeStackScreenProps<StackParamList, AppScreens.Welcome> {}
 
+const { IntegrationModule } = NativeModules;
+
 const WelcomeScreen: React.FunctionComponent<PropTypes> = () => {
   const isDarkMode = useColorScheme() === 'dark';
+
+  const openDetailScreen = () => {
+    IntegrationModule.openDetailScreen();
+  };
 
   useEffect(() => {
     setTimeout(() => RNBootSplash.hide({ fade: true }), 3000);
@@ -20,6 +26,7 @@ const WelcomeScreen: React.FunctionComponent<PropTypes> = () => {
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <Text accessibilityRole={'text'}>Welcome Screen</Text>
       <TouchableOpacity
+        onPress = {openDetailScreen}
         testID="dummy-button"
         accessibilityState={{ disabled: false }}
         accessibilityRole={'button'}>
