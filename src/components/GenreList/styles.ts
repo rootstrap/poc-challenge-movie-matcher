f3f543ab42588a982d5d0d19fc50accd69aa85c2
ramirogavagnin <ramirogavagnin@gmail.com>
2022-11-17 17:06:59 -0300

    Fix genre list text styles

--- src/components/GenreList/styles.ts
+++ src/components/GenreList/styles.ts
@@ -4,22 +4,24 @@ import { BLACK } from 'constants/colors';
 
 export default StyleSheet.create({
   listContainer: {
-    maxHeight: 30,
+    maxHeight: 22,
     marginTop: 7,
   },
   listContent: {
     padding: 0,
   },
   genreText: {
-    maxHeight: 30,
+    maxHeight: 22,
     borderRadius: 4,
     borderWidth: 1,
     borderColor: BLACK,
     color: BLACK,
     padding: 4,
+    fontSize: 10,
+    fontWeight: '400',
   },
   moreGenresContainer: {
-    height: 30,
+    height: 22,
     justifyContent: 'center',
     alignItems: 'center',
     marginLeft: 5,
