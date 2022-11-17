3547fa0f1cf98d00a538996ef1ad4388b945f086
aguskoll <aguskoll@gmail.com>
2022-11-17 16:18:19 -0300

    wip parametros

--- android/app/src/main/java/com/moviematcher/IntegrationModule.java
+++ android/app/src/main/java/com/moviematcher/IntegrationModule.java
@@ -6,9 +6,12 @@ import android.util.Log;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
 import com.moviematcher.customView.asd.MovieDetailActivity2;
 
 public class IntegrationModule extends ReactContextBaseJavaModule {
+    public String OVERVIEW="overview";
+    public String GENRES = "genres";
 
     IntegrationModule(ReactApplicationContext context) {
         super(context);
@@ -19,11 +22,14 @@ public class IntegrationModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void openDetailScreen(){
+    public void openDetailScreen(ReadableMap readableMap){
+        String overview = readableMap.getString("overview");
+
         Log.d("IntegrationModule", "open detail activity");
         ReactApplicationContext context = getReactApplicationContext();
         Intent intent = new Intent(context, MovieDetailActivity2.class);
-        intent.putExtra("title", "Movie name");
+        intent.putExtra(OVERVIEW, readableMap.getString(OVERVIEW));
+        //intent.putStringArrayListExtra(GENRES, readableMap.get("genres"));
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
     }
